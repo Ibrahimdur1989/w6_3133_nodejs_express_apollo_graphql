509a58772127a9bdccdb763922d2074413cdc1bd
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2026-02-22 17:06:56 -0500

    Add movie queries

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,20 @@
 import MovieModel from '../models/movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
+const movieResolvers = {
+    Query: {
+        movies: async () => {
+            return await MovieModel.find();
+        },
+
+        movie: async (__, args) => {
+            return await MovieModel.findById(args.id);
+        },
+
+        moviesByDirector: async (__dirname, args) => {
+            return await MovieModel.findByDirector(args.director_name)
+        }
+    }
+};
+
+export default movieResolvers;
